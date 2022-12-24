5df1ff88df33e740d3429d2ead3cef4290ecd1c3
Chris Deaton <dcdeaton@ncsu.edu>
2022-12-23 21:58:05 -0500

    Drop Expires header Made redundant by the Cache-Control header

--- header.php
+++ header.php
@@ -84,7 +84,6 @@ require_once 'scripts/get_lib.php';
 //---------------------Headers' header-----------------------------------------
 // sets encoding defined in config for language support
 header('Content-Type: text/html; charset='.$site_encoding);
-header('Expires: Tue, 01 Jan 2000 00:00:00 GMT');
 header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
