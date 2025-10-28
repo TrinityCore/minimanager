e95fea6b197adf1c005af5221254d01d2291c4b7
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2025-10-28 22:37:10 +0200

    Initial talent tree update

--- char_talent.php
+++ char_talent.php
@@ -38,6 +38,16 @@ function char_talent(&$sqlr, &$sqlc)
         if (($user_lvl > $owner_gmlvl)||($owner_name === $user_name))
         {
             $result = $sqlc->query('SELECT spell FROM character_spell WHERE guid = '.$id.' and active = 1 and disabled = 0 ORDER BY spell DESC');
+
+            $output .= '
+                    <style type="text/css">
+                        img.grayscale {
+                            filter: grayscale(100%);
+                            -webkit-filter: grayscale(100%);
+                        }
+                    </style>';
+            // ---------------------------------------------------------------------------------------
+
             $output .= '
                         <center>
                             <div id="tab_content">
@@ -61,67 +71,110 @@ function char_talent(&$sqlr, &$sqlc)
                 $sqlm->connect($mmfpm_db['addr'], $mmfpm_db['user'], $mmfpm_db['pass'], $mmfpm_db['name']);
 
                 $tabs = [];
-                $l = 0;
+                $tabs_temp = []; 
+                $l = 0; //
+                $talent_tabs_ids = [];
 
-                while (($talent = $sqlc->fetch_assoc($result)) && ($l < $talent_points_used))
+                while ($talent = $sqlc->fetch_assoc($result))
                 {
-                    if ($tab = $sqlm->fetch_assoc($sqlm->query('SELECT field_1, field_2, field_3, field_13, field_16 from dbc_talent where field_8 = '.$talent['spell'].' LIMIT 1')))
+                    $talent_spell_id = $talent['spell'];
+
+                    $query = 'SELECT id, field_1, field_2, field_3, field_13, field_16, field_4, field_5, field_6, field_7, field_8 '.
+                             'FROM dbc_talent '.
+                             'WHERE field_4 = '.$talent_spell_id.' OR field_5 = '.$talent_spell_id.' OR field_6 = '.$talent_spell_id.' OR field_7 = '.$talent_spell_id.' OR field_8 = '.$talent_spell_id.' LIMIT 1';
+
+                    if ($tab = $sqlm->fetch_assoc($sqlm->query($query)))
                     {
-                        if (isset($tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']]))
-                            $l -=$tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']][1];
-                        $tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']] = [$talent['spell'], '5', '5'];
-                        $l += 5;
+                        $talent_tabs_ids[(int)$tab['field_1']] = true;
+
+                        $rank_put = 0;
+                        if (isset($tab['field_8']) && $tab['field_8'] == $talent_spell_id)
+                            $rank_put = 5;
+                        elseif (isset($tab['field_7']) && $tab['field_7'] == $talent_spell_id)
+                            $rank_put = 4;
+                        elseif (isset($tab['field_6']) && $tab['field_6'] == $talent_spell_id)
+                            $rank_put = 3;
+                        elseif (isset($tab['field_5']) && $tab['field_5'] == $talent_spell_id)
+                            $rank_put = 2;
+                        elseif (isset($tab['field_4']) && $tab['field_4'] == $talent_spell_id)
+                            $rank_put = 1;
+
+                        if ($rank_put === 0)
+                            continue;
+
+                        $talent_key = $tab['field_1'].'_'.$tab['field_2'].'_'.$tab['field_3'];
+
+                        $l += 1; 
+
+                        if (!isset($tabs_temp[$talent_key]) || $rank_put > $tabs_temp[$talent_key]['rank'])
+                        {
+
+                            $max_rank = 0;
+                            for ($r = 4; $r <= 8; $r++) {
+                                $field_name = 'field_' . $r;
+                                if (isset($tab[$field_name]) && (int)$tab[$field_name] > 0) {
+                                    $max_rank++;
+                                }
+                            }
+
+                            $icon_border_color = ((int)$rank_put === $max_rank) ? '5' : '2'; 
+
+                            $tabs_temp[$talent_key] = [
+                                'spell' => $talent_spell_id, 
+                                'rank' => $rank_put, 
+                                'color' => $icon_border_color,
+                                'max_rank' => $max_rank,
+                                'id_talent' => (int)$tab['id']
+                            ];
+                        }
 
                         if ($tab['field_13'])
-                            talent_dependencies($tabs, $tab, $l, $sqlm);
-                    }
-                    elseif ($tab = $sqlm->fetch_assoc($sqlm->query('SELECT field_1, field_2, field_3, field_13, field_16, field_8 from dbc_talent where field_7 = '.$talent['spell'].' LIMIT 1')))
-                    {
-                        if (isset($tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']]))
-                            $l -=$tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']][1];
-
-                        $tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']] = [$talent['spell'], '4', ($tab['field_8'] ? '2' : '5')];
-                        $l += 4;
-
-                        if ($tab['field_13'])
-                            talent_dependencies($tabs, $tab, $l, $sqlm);
-                    }
-                    elseif ($tab = $sqlm->fetch_assoc($sqlm->query('SELECT field_1, field_2, field_3, field_13, field_16, field_7 from dbc_talent where field_6 = '.$talent['spell'].' LIMIT 1')))
-                    {
-                        if (isset($tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']]))
-                            $l -=$tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']][1];
-
-                        $tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']] = [$talent['spell'],'3', ($tab['field_7'] ? '2' : '5')];
-                        $l += 3;
-
-                        if ($tab['field_13'])
-                            talent_dependencies($tabs, $tab, $l, $sqlm);
-                    }
-                    elseif ($tab = $sqlm->fetch_assoc($sqlm->query('SELECT field_1, field_2, field_3, field_13, field_16, field_6 from dbc_talent where field_5 = '.$talent['spell'].' LIMIT 1')))
-                    {
-                        if (isset($tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']]))
-                            $l -=$tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']][1];
-
-                        $tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']] = [$talent['spell'],'2', ($tab['field_6'] ? '2' : '5')];
-                        $l += 2;
-
-                        if ($tab['field_13'])
-                            talent_dependencies($tabs, $tab, $l, $sqlm);
-                    }
-                    elseif ($tab = $sqlm->fetch_assoc($sqlm->query('SELECT field_1, field_2, field_3, field_13, field_16, field_5 from dbc_talent where field_4 = '.$talent['spell'].' LIMIT 1')))
-                    {
-                        if (isset($tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']]))
-                            $l -=$tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']][1];
-
-                        $tabs[$tab['field_1']][$tab['field_2']][$tab['field_3']] = [$talent['spell'],'1', ($tab['field_5'] ? '2' : '5')];
-                        $l += 1;
-
-                        if ($tab['field_13'])
-                            talent_dependencies($tabs, $tab, $l, $sqlm);
+                            talent_dependencies($tabs_temp, $tab, $sqlm);
                     }
                 }
                 unset($tab);
                 unset($talent);
+
+                if (!empty($talent_tabs_ids)) {
+                    $talent_tabs_list = implode(',', array_keys($talent_tabs_ids));
+
+                    $query_all_talents = 'SELECT id, field_1, field_2, field_3, field_4, field_5, field_6, field_7, field_8, field_16 '.
+                                         'FROM dbc_talent '.
+                                         'WHERE field_1 IN ('.$talent_tabs_list.')';
+
+                    $result_all = $sqlm->query($query_all_talents);
+
+                    while ($all_tab = $sqlm->fetch_assoc($result_all)) {
+                        $talent_key = $all_tab['field_1'].'_'.$all_tab['field_2'].'_'.$all_tab['field_3'];
+
+                        if (!isset($tabs_temp[$talent_key])) {
+                            $max_rank_all = 0;
+                            for ($r = 4; $r <= 8; $r++) {
+                                $field_name = 'field_' . $r;
+                                if (isset($all_tab[$field_name]) && (int)$all_tab[$field_name] > 0) {
+                                    $max_rank_all++;
+                                }
+                            }
+
+                            $tabs_temp[$talent_key] = [
+                                'spell' => (int)$all_tab['field_4'],
+                                'rank' => 0, 
+                                'color' => '',
+                                'max_rank' => $max_rank_all,
+                                'id_talent' => (int)$all_tab['id']
+                            ];
+                        }
+                    }
+                    unset($result_all);
+                }
+
+                ksort($tabs_temp); 
+                foreach ($tabs_temp as $key => $data) {
+                    list($field_1, $field_2, $field_3) = explode('_', $key);
+                    $tabs[(int)$field_1][(int)$field_2][(int)$field_3] = [$data['spell'], $data['rank'], $data['color']];
+                }
+                unset($tabs_temp);
+
                 foreach ($tabs as $k=>$data)
                 {
                     $points = 0;
@@ -139,13 +192,20 @@ function char_talent(&$sqlr, &$sqlc)
                         {
                             if(isset($data[$i][$j]))
                             {
+                                $grayscale_class = ((int)$data[$i][$j][1] === 0) ? ' grayscale' : ''; 
+
                                 $output .= '
                                                     <td valign="bottom" align="center" style="border-top-width: 0px;border-bottom-width: 0px;">
                                                         <a href="'.$spell_datasite.$data[$i][$j][0].'" target="_blank">
-                                                            <img src="'.spell_get_icon($data[$i][$j][0], $sqlm).'" width="36" height="36" class="icon_border_'.$data[$i][$j][2].'" alt="" />
-                                                        </a>
+                                                            <img src="'.spell_get_icon($data[$i][$j][0], $sqlm).'" width="36" height="36" class="icon_border'.($data[$i][$j][2] === '' ? '' : '_'.$data[$i][$j][2]).$grayscale_class.'" alt="" />
+                                                        </a>';
+                                if ($data[$i][$j][1] > 0)
+                                {
+                                    $output .= '
                                                         <div style="width:0px;margin:-14px 0px 0px 30px;font-size:14px;color:black">'.$data[$i][$j][1].'</div>
-                                                        <div style="width:0px;margin:-14px 0px 0px 29px;font-size:14px;color:white">'.$data[$i][$j][1].'</div>
+                                                        <div style="width:0px;margin:-14px 0px 0px 29px;font-size:14px;color:white">'.$data[$i][$j][1].'</div>';
+                                }
+                                $output .= '
                                                     </td>';
                                 $points += $data[$i][$j][1];
                             }
@@ -242,7 +302,7 @@ function char_talent(&$sqlr, &$sqlc)
             $output .='
                             <br />
                         </center>
-                        <!-- end of char_talent.php -->';
+                        ';
         }
         else
             error($lang_char['no_permission']);
@@ -252,17 +312,37 @@ function char_talent(&$sqlr, &$sqlc)
 }
 
 
-function talent_dependencies(&$tabs, &$tab, &$i, &$sqlm)
+function talent_dependencies(&$tabs_temp, &$tab, &$sqlm)
 {
-    if ($dep = $sqlm->fetch_assoc($sqlm->query('SELECT field_1, field_2, field_3, field_'.($tab['field_16'] + 1).', field_13,field_16'.(($tab['field_16'] < 4) ? ', field_'.($tab['field_16'] + 2).'' : '').' from dbc_talent where id = '.$tab['field_13'].' and field_'.($tab['field_16'] + 1).' != 0 LIMIT 1')))
+    $dep_query = 'SELECT id, field_1, field_2, field_3, field_13, field_16, field_4, field_5, field_6, field_7, field_8 '.
+                 'FROM dbc_talent '.
+                 'WHERE id = '.$tab['field_13'].' LIMIT 1';
+
+    if ($dep = $sqlm->fetch_assoc($sqlm->query($dep_query)))
     {
-        if(empty($tabs[$dep['field_1']][$dep['field_2']][$dep['field_3']]))
+        $dep_max_rank = 0;
+        for ($r = 4; $r <= 8; $r++) {
+            $field_name = 'field_' . $r;
+            if (isset($dep[$field_name]) && (int)$dep[$field_name] > 0) {
+                $dep_max_rank++;
+            }
+        }
+        $dep_spell_id_rank1 = (int)$dep['field_4']; 
+
+        $dep_key = $dep['field_1'].'_'.$dep['field_2'].'_'.$dep['field_3'];
+
+        if(!isset($tabs_temp[$dep_key])) 
         {
-            $tabs[$dep['field_1']][$dep['field_2']][$dep['field_3']] = [$dep['field_'.($tab['field_16'] + 1).''], ''.($tab['field_16'] + 1).'', (($tab['field_16'] < 4) ? ($dep['field_'.($tab['field_16'] + 2).''] ? '2' : '5') : '5')];
-            $i += ($tab['field_16'] + 1);
+            $tabs_temp[$dep_key] = [
+                'spell' => $dep_spell_id_rank1, 
+                'rank' => 0, 
+                'color' => '',
+                'max_rank' => $dep_max_rank,
+                'id_talent' => (int)$dep['id']
+            ];
 
             if ($dep['field_13'])
-                talent_dependencies($tabs, $dep, $i, $sqlm);
+                talent_dependencies($tabs_temp, $dep, $sqlm);
         }
     }
 }
@@ -273,7 +353,7 @@ function talent_dependencies(&$tabs, &$tab, &$i, &$sqlm)
 //########################################################################################################################
 
 // action variable reserved for future use
-//$action = (isset($_GET['action'])) ? $_GET['action'] : NULL;
+// $action = (isset($_GET['action'])) ? $_GET['action'] : NULL;
 
 $lang_char = lang_char();
 
